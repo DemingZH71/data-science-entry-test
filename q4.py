87aaa76ebb8d2a24a58417207860e7f338505dce
DemingZH71 <deming.zhang71@gmail.com>
2025-03-30 18:06:16 +0800

    Add files via upload

--- q4.py
+++ q4.py
@@ -3,8 +3,6 @@ def string_reverse(s):                  # Create function.
 
     if isinstance(s, str):              # Check if the given argument is string.
         return s[::-1]                  # If YES, reverse it by string SLICING, then return.
-    else:
-        return -1                       # Return -1 if s is not string.
 
 string_reverse("Hello World")           # Invoke the function "string_reverse" using: "Hello World"
 string_reverse("Python")                # Invoke the function "string_reverse" using: "Python"
--- q6.py
+++ q6.py
@@ -4,13 +4,10 @@ def find_first_negative(lst):       # Task 1: Create function.
 
         if lst[i] < 0:              # Check if value is negative.
            return lst[i]            # Return the negative value.
-        else:
-           i += 1
-           continue
+        i += 1
 
     return "No negatives"           # Return "No negatives" if no negative value found.
 
-tst1 = [3, 5, -1, 7, -2, 8]
-print(find_first_negative(tst1))    # Task 2: Invoke the function.
-tst2 = [2, 10, 7, 0]
-print(find_first_negative(tst2))    # Task 2: Invoke the function.
+find_first_negative([3, 5, -1, 7, -2, 8])    # Task 2: Invoke the function.
+
+find_first_negative([2, 10, 7, 0])    # Task 2: Invoke the function.
